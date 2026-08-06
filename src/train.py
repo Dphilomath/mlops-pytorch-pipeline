f348b4c16c07f7556bdfc98b211de4b8cb8f5f66
Daniyal <daniyalmahmood123@gmail.com>
2026-08-07 00:09:50 +0530

    fix dataset module path

--- src/train.py
+++ src/train.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 import yaml
-from dataset import get_dataloaders
+from src.dataset import get_dataloaders
 from model import get_model
 
 def load_config(config_path: str) -> dict:
